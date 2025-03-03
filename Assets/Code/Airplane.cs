dbc9c79539c586eaa8c50c5278a14ca3451a7386
teeradet025 <teeradet.math@gmail.com>
2025-03-03 11:38:08 +0700

    Update Airplane.cs

--- Assets/Code/Airplane.cs
+++ Assets/Code/Airplane.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class Airplane : MonoBehaviour
 {
-    
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+    [SerializeField] float YawPower = 50f;
+    [SerializeField] float PitchPower = 50f;
+    [SerializeField] float RollPower = 50f;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +22,20 @@ public class Airplane : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-
+            rb.AddForce(transform.forward * enginePower);
         }
+        Vector3 lift =Vector3.Project(rb.linearVelocity,transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.angularVelocity.magnitude * angularDrag;
+
+        //float yaw = Input.GetAxis("Horizontal") * 
+        //float pich = Input.GetAxis("Vertical") *
+        //float roll = Input.GetAxis("Roll") *
+
+        //rb.AddTorque(transform.up * yaw);
+        //rb.AddTorque(transform.right * pitch);
+        //rb.AddTorque(transform.forward * roll);
     }
 }
